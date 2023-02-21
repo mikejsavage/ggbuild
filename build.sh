38e3cf876d1c70286d95bc0ad109513d17b2e941
Michael Savage <mike@mikejsavage.co.uk>
2023-02-21 15:14:35 +0200

    Fix build.sh downloading zig when you already have it

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 zig_version="0.10.1"
 zig="zig-windows-x86_64-$zig_version"
 
-if [ ! -e "$zig/zig" ]; then
+if [ ! -e "$zig/zig.exe" ]; then
 	wget "https://ziglang.org/download/$zig_version/$zig.zip"
 	tar xf "$zig.tar.xz"
 fi
